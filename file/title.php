37831372b391254c4f7969de99188bff4cf30a4b
Arun S <arunsmullappallil@gmail.com>
2022-01-04 00:40:32 +0530

    Add files via upload

---
+++ file/title.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Auto Spa</title>
+	 <link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="top-bar">
+	 	<div class="logo">
+	 	<img src="./img/logo.png" alt="logo" width="150px" height="150px">	 		
+        </div>
+            <div class="top-bar-text">
+              <h3>Opening Hour:-</h3><br>
+              <p>Mon - Fri, 8:00am - 9:00pm</p>                                 
+              <h3>Call Us:-</h3><br>
+              <p style="padding: 50px">+012 345 6789</p>                               
+              <h3>Email Us:-</h3><br>
+              <p style="padding: 35px">info@example.com</p>
+            </div>                              
+    </div>
+         <ul class="nav-bar">
+      	 <li><a href="" class="active">Home</a></li>
+         <li><a href="">About</a></li>
+         <li><a href="">Service</a></li>
+         <li><a href="">Price</a></li>
+         <li><a href="">Booking</a></li>
+         <li><a href="">Contact</a></li>
+         <li style="float:right"><a href=""><button class="w3-button w3-round-xlarge">
+         Get Appointment</button></a></li>
+         </ul>
+
+       
+         
+</body> 
+</html>
